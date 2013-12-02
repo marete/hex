6e4f99a7c986ed9220d8e7518caf2c6e599d7853
Brian Gitonga Marete <marete@toshnix.com>
2013-12-02 15:28:29 +0300

    Initial import.

---
+++ README.md
@@ -0,0 +1,11 @@
+hex
+====
+
+`hex` is a small utility to decode from and encode to hexadecimal encoding as the encoding is defined and supported by the Go [encoding/hex](http://golang.org/pkg/encoding/hex/) package.
+
+Usage
+---------
+`hex` reads input from standard input and writes output to standard output.
+
+To decode: `cat foo.hex | hex -d`
+To encode: `cat foo.bin | hex`
---
+++ hex/hex.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
+
+var dec bool // Decode instead of encode
+
+func init() {
+	flag.BoolVar(&dec, "d", false, "decode instead of encoding")
+}
+
+func encode() {
+	fmt.Fprintln(os.Stderr, "ERROR: Not implemented")
+}
+
+func decode() {
+	var in [4096]byte
+	var out [4096 / 2]byte // The output of the hex encoding yields is 1/2 the length of the input
+
+	for {
+		n, err := os.Stdin.Read(in[:])
+		l := n
+		// n should be a multiple of 2. What to do now if it is not?
+		if n%2 != 0 {
+			// We try and read an extra byte
+			_, err := os.Stdin.Read(in[n : n+1])
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "ERROR: decode(): Input must contain an even number of bytes")
+				os.Exit(1)
+			}
+			l += 1
+		}
+
+		// Process what we've got so far
+		m, dErr := hex.Decode(out[:], in[:l])
+		// Write what we've got first, check any decode error later
+		_, wErr := os.Stdout.Write(out[:m])
+		if wErr != nil {
+			fmt.Fprintf(os.Stderr, "ERROR: decode(): %v\n", wErr)
+			os.Exit(1)
+		}
+		if dErr != nil {
+			fmt.Fprintf(os.Stderr, "ERROR: decode(): %v\n", dErr)
+			os.Exit(1)
+		}
+
+		// Finally handle any error from the Read() operation
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				fmt.Fprintf(os.Stderr, "ERROR: decode(): %v\n",
+					err)
+			}
+		}
+
+	}
+}
+
+func main() {
+	flag.Parse()
+	if dec {
+		decode()
+	} else {
+		encode()
+	}
+}
